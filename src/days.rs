ff0330fa8e3cefaad2b09cebe3394ba49f164d5f
Nathan Glover <nathanielsglover@gmail.com>
2022-04-14 19:15:49 -0400

    Solve day 3

--- src/days.rs
+++ src/days.rs
@@ -69,97 +69,116 @@ fn day2(input: &Vec<String>) -> Result<(String, String), String> {
   return Ok(((h1 * d1).to_string(), (h2 * d2).to_string()));
 }
 
-fn day3(_input: &Vec<String>) -> Result<(String, String), String> {
-  return Err(String::from("Not implemented."));
+fn day3(input: &Vec<String>) -> Result<(String, String), String> {
+  const BIT_WIDTH: usize = 12;
+
+  fn bits_to_num(bits: &String) -> u32 { u32::from_str_radix(bits.as_str(), 2).unwrap() }
+
+  let chars = input.into_iter().map(|s| s.chars().collect::<Vec<char>>()).collect::<Vec<Vec<char>>>();
+
+  let mut gamma_rate: Vec<char> = Vec::new();
+  let mut epsilon_rate: Vec<char> = Vec::new();
+
+  fn bit_counts(collection: &Vec<Vec<char>>, index: usize) -> (u32, u32) {
+    let n_zeros = collection.iter().fold(0, |acc, cs| acc + if cs[index] == '0' { 1 } else { 0 });
+    return (n_zeros, (collection.len() as u32) - n_zeros);
+  }
+
+  for i in 0..BIT_WIDTH {
+    let (n_zeros, n_ones) = bit_counts(&chars, i);
+
+    gamma_rate.push(if n_zeros > n_ones { '0' } else { '1' });
+    epsilon_rate.push(if n_zeros < n_ones { '0' } else { '1' });
+  }
+
+  let gamma_str = gamma_rate.into_iter().collect::<String>();
+  let epsilon_str = epsilon_rate.into_iter().collect::<String>();
+  let power_consumption = bits_to_num(&gamma_str) * bits_to_num(&epsilon_str);
+
+  fn filter(chars1: &Vec<Vec<char>>, chars2: &Vec<Vec<char>>, index: usize) -> (Vec<char>, Vec<char>) {
+    if (chars1.len() == 1 && chars2.len() == 1) || index >= BIT_WIDTH {
+      return (chars1[0].clone(), chars2[0].clone());
+    }
+
+    fn get_filtered(collection: &Vec<Vec<char>>, index: usize, predicate: impl Fn(char) -> bool) -> Vec<Vec<char>> {
+      return collection.iter().filter(|cs| predicate(cs[index])).cloned().collect::<Vec<Vec<char>>>();
+    }
+
+    let (rr1, rr2): (&Vec<Vec<char>>, &Vec<Vec<char>>);
+    let (r1, r2): (Vec<Vec<char>>, Vec<Vec<char>>);
+
+    if chars1.len() == 1 {
+      rr1 = chars1;
+    } else {
+      let (n_zeros, n_ones) = bit_counts(&chars1, index);
+      let most_common = if n_zeros > n_ones { '0' } else { '1' };
+      r1 = get_filtered(&chars1, index, |c| c == most_common);
+      rr1 = &r1;
+    }
+
+    if chars2.len() == 1 {
+      rr2 = chars2;
+    } else {
+      let (n_zeros, n_ones) = bit_counts(&chars2, index);
+      let least_common = if n_ones < n_zeros { '1' } else { '0' };
+      r2 = get_filtered(&chars2, index, |c| c == least_common);
+      rr2 = &r2;
+    }
+
+    return filter(rr1, rr2, index + 1);
+  }
+
+  let (oxygen_generator_rating, co2_scrubber_rating) = filter(&chars, &chars, 0);
+  let oxygen_generator_string = oxygen_generator_rating.into_iter().collect::<String>();
+  let co2_scrubber_string = co2_scrubber_rating.into_iter().collect::<String>();
+  let life_support_rating = bits_to_num(&oxygen_generator_string) * bits_to_num(&co2_scrubber_string);
+
+  return Ok((power_consumption.to_string(), life_support_rating.to_string()));
 }
 
-fn day4(_input: &Vec<String>) -> Result<(String, String), String> {
-  return Err(String::from("Not implemented."));
-}
+fn day4(_input: &Vec<String>) -> Result<(String, String), String> { return Err(String::from("Not implemented.")); }
 
-fn day5(_input: &Vec<String>) -> Result<(String, String), String> {
-  return Err(String::from("Not implemented."));
-}
+fn day5(_input: &Vec<String>) -> Result<(String, String), String> { return Err(String::from("Not implemented.")); }
 
-fn day6(_input: &Vec<String>) -> Result<(String, String), String> {
-  return Err(String::from("Not implemented."));
-}
+fn day6(_input: &Vec<String>) -> Result<(String, String), String> { return Err(String::from("Not implemented.")); }
 
-fn day7(_input: &Vec<String>) -> Result<(String, String), String> {
-  return Err(String::from("Not implemented."));
-}
+fn day7(_input: &Vec<String>) -> Result<(String, String), String> { return Err(String::from("Not implemented.")); }
 
-fn day8(_input: &Vec<String>) -> Result<(String, String), String> {
-  return Err(String::from("Not implemented."));
-}
+fn day8(_input: &Vec<String>) -> Result<(String, String), String> { return Err(String::from("Not implemented.")); }
 
-fn day9(_input: &Vec<String>) -> Result<(String, String), String> {
-  return Err(String::from("Not implemented."));
-}
+fn day9(_input: &Vec<String>) -> Result<(String, String), String> { return Err(String::from("Not implemented.")); }
 
-fn day10(_input: &Vec<String>) -> Result<(String, String), String> {
-  return Err(String::from("Not implemented."));
-}
+fn day10(_input: &Vec<String>) -> Result<(String, String), String> { return Err(String::from("Not implemented.")); }
 
-fn day11(_input: &Vec<String>) -> Result<(String, String), String> {
-  return Err(String::from("Not implemented."));
-}
+fn day11(_input: &Vec<String>) -> Result<(String, String), String> { return Err(String::from("Not implemented.")); }
 
-fn day12(_input: &Vec<String>) -> Result<(String, String), String> {
-  return Err(String::from("Not implemented."));
-}
+fn day12(_input: &Vec<String>) -> Result<(String, String), String> { return Err(String::from("Not implemented.")); }
 
-fn day13(_input: &Vec<String>) -> Result<(String, String), String> {
-  return Err(String::from("Not implemented."));
-}
+fn day13(_input: &Vec<String>) -> Result<(String, String), String> { return Err(String::from("Not implemented.")); }
 
-fn day14(_input: &Vec<String>) -> Result<(String, String), String> {
-  return Err(String::from("Not implemented."));
-}
+fn day14(_input: &Vec<String>) -> Result<(String, String), String> { return Err(String::from("Not implemented.")); }
 
-fn day15(_input: &Vec<String>) -> Result<(String, String), String> {
-  return Err(String::from("Not implemented."));
-}
+fn day15(_input: &Vec<String>) -> Result<(String, String), String> { return Err(String::from("Not implemented.")); }
 
-fn day16(_input: &Vec<String>) -> Result<(String, String), String> {
-  return Err(String::from("Not implemented."));
-}
+fn day16(_input: &Vec<String>) -> Result<(String, String), String> { return Err(String::from("Not implemented.")); }
 
-fn day17(_input: &Vec<String>) -> Result<(String, String), String> {
-  return Err(String::from("Not implemented."));
-}
+fn day17(_input: &Vec<String>) -> Result<(String, String), String> { return Err(String::from("Not implemented.")); }
 
-fn day18(_input: &Vec<String>) -> Result<(String, String), String> {
-  return Err(String::from("Not implemented."));
-}
+fn day18(_input: &Vec<String>) -> Result<(String, String), String> { return Err(String::from("Not implemented.")); }
 
-fn day19(_input: &Vec<String>) -> Result<(String, String), String> {
-  return Err(String::from("Not implemented."));
-}
+fn day19(_input: &Vec<String>) -> Result<(String, String), String> { return Err(String::from("Not implemented.")); }
 
-fn day20(_input: &Vec<String>) -> Result<(String, String), String> {
-  return Err(String::from("Not implemented."));
-}
+fn day20(_input: &Vec<String>) -> Result<(String, String), String> { return Err(String::from("Not implemented.")); }
 
-fn day21(_input: &Vec<String>) -> Result<(String, String), String> {
-  return Err(String::from("Not implemented."));
-}
+fn day21(_input: &Vec<String>) -> Result<(String, String), String> { return Err(String::from("Not implemented.")); }
 
-fn day22(_input: &Vec<String>) -> Result<(String, String), String> {
-  return Err(String::from("Not implemented."));
-}
+fn day22(_input: &Vec<String>) -> Result<(String, String), String> { return Err(String::from("Not implemented.")); }
 
-fn day23(_input: &Vec<String>) -> Result<(String, String), String> {
-  return Err(String::from("Not implemented."));
-}
+fn day23(_input: &Vec<String>) -> Result<(String, String), String> { return Err(String::from("Not implemented.")); }
 
-fn day24(_input: &Vec<String>) -> Result<(String, String), String> {
-  return Err(String::from("Not implemented."));
-}
+fn day24(_input: &Vec<String>) -> Result<(String, String), String> { return Err(String::from("Not implemented.")); }
 
-fn day25(_input: &Vec<String>) -> Result<(String, String), String> {
-  return Err(String::from("Not implemented."));
-}
+fn day25(_input: &Vec<String>) -> Result<(String, String), String> { return Err(String::from("Not implemented.")); }
 
 pub fn solve_day(day: u8, input: Lines<BufReader<File>>) -> Result<(String, String), String> {
   let lines = &input.map(|opt| opt.unwrap()).collect();
